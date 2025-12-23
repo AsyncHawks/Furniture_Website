97417d992a730ac6190fb1ae4c68c3db5e55c006
ahad-199 <ahada1249@gmail.com>
2025-12-23 14:27:21 +0500

    Collection Functionality

--- Furniture_Web/Furniture_Web/settings.py
+++ Furniture_Web/Furniture_Web/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'accounts',
     'shop',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- Furniture_Web/shop/models/product_image.py
+++ Furniture_Web/shop/models/product_image.py
@@ -2,7 +2,7 @@ from django.db import models
 from shop.models.product import Product
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')  # Link to Product
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_images')  # Link to Product
     image = models.ImageField(upload_to='product_images/')
 
     def __str__(self):
